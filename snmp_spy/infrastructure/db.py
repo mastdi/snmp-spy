50584b46d93c7ea0b10e9b3c0b97d1b53375fdf3
Martin Storgaard <ms@ynk.gl>
2022-09-01 22:01:59 +0200

    Added doc-strings to db.py

--- snmp_spy/infrastructure/db.py
+++ snmp_spy/infrastructure/db.py
@@ -1,8 +1,9 @@
 import uuid
-from typing import Any, Callable, Optional, Type
+from typing import Any, Callable, Optional, Type, Union
 
 import sqlalchemy
 import sqlalchemy_utils
+from sqlalchemy.engine import URL
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     AsyncSessionTransaction,
@@ -17,6 +18,10 @@ Base: DeclarativeMeta = declarative_base()
 
 
 def primary_key_column() -> sqlalchemy.Column:
+    """Constructs the primary key column.
+
+    :return: The primary key column.
+    """
     return sqlalchemy.Column(
         "identifier", sqlalchemy_utils.UUIDType(), primary_key=True, default=uuid.uuid4
     )
@@ -26,11 +31,24 @@ session: Callable[..., AsyncSession]
 
 
 async def init_db(
-    database_url: str,
+    database_url: Union[str, URL],
     echo: bool = False,
     create_all: bool = False,
     expire_on_commit: bool = False,
 ) -> None:
+    """Initialize the session defined in this module.
+
+    :param database_url: Usually a string that indicates database dialect and
+    connection arguments
+    :param echo: if True, the Engine will log all statements as well as a ``repr()`` of
+    their parameter lists to the default log handler, which defaults to ``sys.stdout``
+    for output. If set to the string ``"debug"``, result rows will be printed to the
+    standard output as well. The ``echo`` attribute of ``Engine`` can be modified at
+    any time to turn logging on and off; direct control of logging is also available
+    using the standard Python ``logging`` module.
+    :param create_all: if True, all tables stored in the metadata will be created.
+    :param expire_on_commit: Tells if the session should expire on commit
+    """
     engine = create_async_engine(
         database_url,
         echo=echo,
@@ -51,6 +69,8 @@ async def init_db(
 
 
 class SessionContext:
+    """A context manager to wrap executions in a transaction and rollback on failure."""
+
     def __init__(self) -> None:
         self.__session: Optional[AsyncSession] = None
         self.__active_transaction: Optional[AsyncSessionTransaction] = None
