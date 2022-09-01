d35bd01e9603e923cfebd146fe7b9ed1a2ec0043
Martin Storgaard <ms@ynk.gl>
2022-09-01 22:41:09 +0200

    Using tempfile and removed redundant event loop check in teardown

--- features/environment.py
+++ features/environment.py
@@ -1,33 +1,28 @@
 import asyncio
 import os
+import tempfile
 
 from behave.model import Scenario
 from behave.runner import Context
 
+from snmp_spy.infrastructure.db import init_db
+
 
 def before_scenario(context: Context, scenario: Scenario) -> None:
-    from uuid import uuid4
-
-    from snmp_spy.infrastructure.db import init_db
-
     try:
         loop = asyncio.get_running_loop()
     except RuntimeError:
         loop = asyncio.get_event_loop()
 
-    context.db_name = f"behave-tests-{uuid4()}.db"
-    loop.run_until_complete(
-        init_db(f"sqlite+aiosqlite:///{context.db_name}", False, True)
-    )
+    handle, path = tempfile.mkstemp(suffix=".db", prefix="behave-tests")
+    context.db_name = path
+    loop.run_until_complete(init_db(f"sqlite+aiosqlite:///{path}", False, True))
 
 
 def after_scenario(context: Context, scenario: Scenario) -> None:
     from snmp_spy.infrastructure.db import session
 
-    try:
-        loop = asyncio.get_running_loop()
-    except RuntimeError:
-        loop = asyncio.get_event_loop()
+    loop = asyncio.get_event_loop()
     loop.run_until_complete(session().close())
 
     os.remove(context.db_name)
